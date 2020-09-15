dded918afa6bfd5de9a8f8dbb08763c4ae336105
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-09-15 15:06:24 -0500

    created Form component, added axios.js, and declared updateForm function

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Form from "./Form";
 
 function App() {
   const initialValues = {
@@ -11,6 +12,20 @@ function App() {
 
   const [teamList, setTeamList] = useState([]);
   const [values, setValues] = useState(initialValues);
+
+  const updateForm = (inputName, inputValue) => {
+    setValues({ ...values, [inputName]: inputValue });
+  };
+
+  const submitForm = () => {
+    const newTeammember = {
+      name: values.name.trim(),
+      email: values.email.trim(),
+      role: values.role,
+    };
+  };
+
+  return <Form />;
 }
 
 export default App;
---
+++ team-builder/src/Form.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+export default function Form(props) {
+  const { values, update, submit } = props;
+
+  const onChange = e => {
+    const { name, value } = e.target;
+    update(name, value);
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    submit();
+  };
+
+  return (
+    <form className="form-container" onSubmit={onSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={values.name}
+          onChange={onChange}
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={values.email}
+          onChange={onChange}
+        />
+      </label>
+      <label>
+        Role:
+        <select name="role" value={values.role} onChange={onChange}>
+          <option value="">-- Select your Role --</option>
+          <option value="frontend">Frontend Engineer</option>
+          <option value="backend">Backend Engineer</option>
+          <option value="designer">Designer</option>
+        </select>
+      </label>
+    </form>
+  );
+}
